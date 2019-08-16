a71e148990c993863aaaeab0c3191a01bd6100e7
Vincent Cote-Roy <vincentcr@gmail.com>
2019-08-16 09:06:34 -0400

    _actually_ the content type should be audio/pcm

--- api/src/domain/streaming/streamStore.ts
+++ api/src/domain/streaming/streamStore.ts
@@ -3,7 +3,7 @@ import * as uuid from "uuid/v4";
 import { StreamSource } from "./streamSource";
 import { Mp3SourceProcessor } from "./mp3SourceProcessor";
 
-const SUPPORTED_CONTENT_TYPES = ["audio/mp3", "audio/mpeg"];
+const SUPPORTED_CONTENT_TYPES = ["audio/pcm"];
 
 export class StreamStore {
   private static readonly cache: { [id: string]: StreamSource } = {};
@@ -11,9 +11,7 @@ export class StreamStore {
   public static async create(params: { contentType: string }) {
     const { contentType } = params;
     if (SUPPORTED_CONTENT_TYPES.indexOf(contentType) < 0) {
-      throw new Error(
-        `only mp3 streams are supported for now (got "${contentType}")`
-      );
+      throw new Error(`Unsupported content-type: "${contentType}"`);
     }
     const processor = await Mp3SourceProcessor.create({ stereo: true });
     const id = uuid();
